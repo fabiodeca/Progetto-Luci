5bde62e46db6122cba93fd58da9bfbe5ba648d6a
Rares Butnariu <156614408+Jamal42069@users.noreply.github.com>
2025-04-11 12:40:16 +0200

    Add files via upload

--- verifica_login.php
+++ verifica_login.php
@@ -10,29 +10,29 @@ if ($conn->connect_error) {
     die("Connessione fallita: " . $conn->connect_error);
 }
 
-$email = $_POST['email'];
-$azienda_di_provenienza = $_POST['azienda_di_provenienza'];
-$ora_registrazione = $_POST['ora_registrazione'];
+$codice = $_POST['codice'];
+$ora_registrazione = date("Y-m-d H:i:s");
 
-// Verifica se l'utente esiste
-$sql = "SELECT * FROM utenti WHERE email = ?";
+
+$sql = "SELECT * FROM utenti WHERE codice = ?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $email);
+$stmt->bind_param("s", $codice);
 $stmt->execute();
 $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
     
-    $sql_insert = "INSERT INTO registrazioni (email, azienda_di_provenienza, ora_registrazione) VALUES (?, ?, ?)";
+    $sql_insert = "INSERT INTO registrazioni (codice, ora_registrazione) VALUES (?, ?)";
     $stmt_insert = $conn->prepare($sql_insert);
-    $stmt_insert->bind_param("sss", $email, $azienda_di_provenienza, $ora_registrazione);
+    $stmt_insert->bind_param("ss", $codice, $ora_registrazione);
     $stmt_insert->execute();
     $stmt_insert->close();
 
-    echo "Registrazione completata con successo!";
+    echo "Accesso registrato con successo!";
 } else {
-    header("Location: register.html");
+    header("Location: login_error.html");
+    exit();
 }
 
 $stmt->close();
