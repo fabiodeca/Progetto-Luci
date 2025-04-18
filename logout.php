8bda769645a919f1370fcffc29fb9964ac3bfaba
fabiodeclauser <fabio.declauser@allievi.bearzi.it>
2025-04-18 15:13:53 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,15 +1,16 @@
 <?php
 session_start();
 
-if (!isset($_SESSION["email"])) {
-    session_write_close();
-    header("Location: login.php");
-    exit();
+if (isset($_SESSION['accesso_id'])) {
+    $stmt = $conn->prepare("UPDATE Ospiti SET uscita = NOW() WHERE id = ?");
+    $stmt->bind_param("i", $_SESSION['accesso_id']);
+    $stmt->execute();
+    $stmt->close();
 }
 
-unset($_SESSION["email"]);
-unset($_SESSION["codice"]);
-session_write_close();
-header('Location: ./index.php');
-exit;
+session_unset();
+session_destroy();
+
+header("Location: login.php");
+exit();
 ?>
