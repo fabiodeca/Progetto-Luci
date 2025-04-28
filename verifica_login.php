fb8e19c7af8cc8102cd1e0d0cf772442a789c283
Rares Butnariu <156614408+Jamal42069@users.noreply.github.com>
2025-04-28 16:25:43 +0200

    Add files via upload

--- verifica_login.php
+++ verifica_login.php
@@ -10,10 +10,11 @@ if ($conn->connect_error) {
     die("Connessione fallita: " . $conn->connect_error);
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $email = $_POST['email'];
 $ID_Ospite = $_POST['ID_Ospite'];
 $ingresso = date("Y-m-d H:i:s");
+$uscita = date("Y-m-d H:i:s", strtotime("+1 hour"));
+
 
 $sql = "SELECT * FROM Ospiti WHERE email = ?";
 $stmt = $conn->prepare($sql);
@@ -21,22 +22,28 @@ $stmt->bind_param("s", $email);
 $stmt->execute();
 $result = $stmt->get_result();
 
-$sql_insert = "INSERT INTO Visite (ingresso) VALUES ( ?)";
-    $stmt_insert = $conn->prepare($sql_insert);
-    $stmt_insert->bind_param("s", $ingresso);
-    $stmt_insert->close();
-
-
-    if ($result->num_rows > 0) {
+if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-        if (password_verify($ID_Ospite, $row['ID_Ospite'])) {
+
+    if (password_verify($ID_Ospite, $row['ID_Ospite'])) {
+        $ID_Ospite_DB = $row['ID_Ospite'];
+
+        $sql_ingresso = "INSERT INTO Visite (ingresso, uscita, ID_Ospite) VALUES (?, ?, ?)";
+        $stmt_ingresso = $conn->prepare($sql_ingresso);
+        $stmt_ingresso->bind_param("sss", $ingresso, $uscita, $ID_Ospite_DB);
+
+        if ($stmt_ingresso->execute()) {
             header("Location: logged.html");
         } else {
-            header("Location: login_error.html");
+            echo "Errore durante l'inserimento nella tabella Visite: " . $stmt_ingresso->error;
         }
-        } else {
-        header("Location: register.html");
+
+        $stmt_ingresso->close();
+    } else {
+        header("Location: login_error.html");
     }
+} else {
+    header("Location: register.html");
 }
 
 $stmt->close();
